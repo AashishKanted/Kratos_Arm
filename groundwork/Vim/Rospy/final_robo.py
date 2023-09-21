71f5d45705d04045acf20f16f117738733622e43
Vimarsh <vimarsh244@gmail.com>
2023-09-21 21:55:36 +0530

    change directory structure

---
+++ groundwork/Vim/Rospy/final_robo.py
@@ -0,0 +1,116 @@
+import rospy
+from std_msgs.msg import Int8, Int16
+
+from sensor_msgs.msg import Joy
+
+# UP = both forward = 2
+# DOWN = both backward = 4
+# LEFT = L FOR / R BACK = 8
+# RIGHT = R FOR / L BACK = 16
+# STOP = 32
+
+
+# iindex = 1 - left axis
+# index = 4 - right axis`
+# 
+# `
+
+
+
+rospy.init_node('motor_control', anonymous=True)
+publisher_motors_bevel = rospy.Publisher('/control_motors', Int8, queue_size=10)
+
+publisher_motors_act_1 = rospy.Publisher('/control_motors_act1', Int16, queue_size=10)
+publisher_motors_act_2 = rospy.Publisher('/control_motors_act2', Int16, queue_size=10)
+
+
+publisher_stepper = rospy.Publisher('/control_stepper', Int8, queue_size=10)
+## buttons - 2 
+# buttons - 0
+
+
+useful_data = [0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0]
+useful_data_2 = [0,0,0,0,0,0,0,0]
+
+def callback_fn(data):
+    global useful_data
+    global useful_data_2
+    useful_data = data.axes    
+    useful_data_2 = data.buttons
+
+    # print(useful_data)
+
+
+rospy.Subscriber("/joy", Joy, callback_fn)
+
+data = 0
+rate = rospy.Rate(10)  #update once every second
+
+while not rospy.is_shutdown():   
+    # data = int(input("Enter input: "))  
+    
+    # global useful_data
+    # publisher1.publish(data)
+    
+    # print(useful_data)
+    condition_1 = useful_data[7]
+    condition_2 = useful_data[6]
+    
+    
+    
+    if(condition_1 == 1.0):
+        motor_control = 2
+    elif(condition_1 == -1.0):
+        motor_control = 4
+    elif(condition_2 == 1.0):
+        motor_control = 8
+    elif(condition_2 == -1.0):
+        motor_control = 16
+    else :
+        motor_control = 32 
+    
+    publisher_motors_bevel.publish(motor_control)
+    # print(motor_control)
+    
+    condition_act_1 = useful_data[1]
+    condition_act_2 = useful_data[4]
+    temp_val_1 = 0
+    temp_val_2 = 0
+    
+    stepper_cond_1 = useful_data_2[0]
+    stepper_cond_2 = useful_data_2[2]
+    
+    if(stepper_cond_1 == 1):
+        publisher_stepper.publish(5)
+    elif(stepper_cond_2 == 1):
+        publisher_stepper.publish(10)
+    
+    if(condition_act_1 >= 0):
+        temp_val_1 = int(condition_act_1 * 255)
+    elif(condition_act_1 < 0):
+        temp_val_1 = int(condition_act_1 * 255)
+        
+    if(condition_act_2 >= 0):
+        temp_val_2 = int(condition_act_2 * 255)
+    elif(condition_act_2 < 0):
+        temp_val_2 = int(condition_act_2 * 255)
+    
+    
+    
+    publisher_motors_act_1.publish(temp_val_1)
+    publisher_motors_act_2.publish(temp_val_2)
+    
+    
+    
+    
+
+
+    rate.sleep()
+
+
+
+# data = float(input("Enter input"))  # take input from user for radius
+# # default value is 5 meters
+# bot = Control_motor()
+# bot.user_input = data
+# bot.blink_led()
---
+++ groundwork/Vim/Rospy/test.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+import rospy
+from std_msgs.msg import Int8
+from sensor_msgs.msg import Joy
+# publishes for actuators
+def actuator(act_1, act_2):
+    if act_1>0.5:
+        pub_act.publish(1) # l up
+    elif act_1<-0.5:
+        pub_act.publish(2) # l down
+    if act_2>0.5:
+        pub_act.publish(3) # r up
+    elif act_2<-0.5:
+        pub_act.publish(4) # r down
+# publishes for bevel motors
+def bevel(x, cir, tri, sq):
+    if x==1:
+        pub_bevel.publish(1)
+    elif cir==1:
+        pub_bevel.publish(2)
+    elif tri==1:
+        pub_bevel.publish(3)
+    elif sq==1:
+        pub_bevel.publish(4)
+def callbackfunc(data):
+    x = data.buttons[0]
+    cir = data.buttons[1]
+    tri = data.buttons[2]
+    sq = data.buttons[3]
+    act_1 = data.axes[1] # top and bottom contraint
+    act_2 = data.axes[4] # left and right contraint
+    # checks for non-zero value of buttons
+    if any(data.buttons[0:4]):
+        bevel(x, cir, tri, sq)
+    else:
+        pub_bevel.publish(0)
+    # checks for non-zero value of buttons
+    if act_1!=0.0 or act_2!=0.0:
+        actuator(act_1, act_2)
+    else:
+        pub_act.publish(0)
+rospy.init_node('arm_control' , anonymous = True)
+pub_bevel = rospy.Publisher('controls_bevel', Int8, queue_size=1)
+pub_act = rospy.Publisher('controls_act', Int8, queue_size=1)
+rate = rospy.Rate(10)
+rospy.Subscriber('/joy', Joy, callback=callbackfunc)
+rospy.spin()
+
+
+
+
+

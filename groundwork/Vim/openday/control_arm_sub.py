2cad3f607060a6c71ac01259bd37531de81808a1
Vimarsh <vimarsh244@gmail.com>
2023-11-06 00:26:09 +0530

    twtrejyt

--- groundwork/Vim/openday/control_arm_sub.py
+++ groundwork/Vim/openday/control_arm_sub.py
@@ -1,77 +1,95 @@
 
 #!/usr/bin/env python3
 
+#!/usr/bin/env python3
+
 import rospy
-from std_msgs.msg import Int16MultiArray, Int8
+from std_msgs.msg import Int16MultiArray
 from sensor_msgs.msg import Joy
 
-rospy.init_node('control_arm_actually', anonymous=True)
-
-direction =0
-
-def callback_fn(data):
-    global direction
-    direction= data.data
-
-
-rospy.Subscriber("/arm_gpt", Int8, callback_fn)
-
-xAxis1 = 0
-yAxis1 = 0
-square = 0
-circle = 0
-
-def callback(data):
-    global xAxis1, yAxis1, square, circle
-
-    xAxis1 = data.axes[0] # axes[0]  //controls Linear Actuators
-    yAxis1 = data.axes[1] # axes[1]
-
-    square = data.buttons[3] #buttons[3] //stepper on
-    circle = data.buttons[1] # buttons[1] //stepper off
-
-pub = rospy.Publisher("/control1", Int16MultiArray, queue_size=1)
-
-def run_gpt():
-    var = 1
-    vel = 150
-    msg.data = [var, vel] 
-    pub.publish(msg)
-    rospy.sleep(2)
-    var = 2
-    msg.data = [var, vel] 
-    pub.publish(msg)
-    rospy.sleep(2)
+class JoystickControl:
+    def __init__(self):
+        self.vel_la1 = 0 # velocity of linear actuator 1 
+        self.vel_la2 = 0 # velocity of linear actuator 2
+        self.var_st = 0 # variable to enabel/disable stepper 
+        self.msg = Int16MultiArray()
+        
+        
+        
+        self.direction = 0
+        self.gpt_input = 0
+        
+        self.sub = rospy.Subscriber("/joy", Joy, self.callback)
+        self.pub = rospy.Publisher("/control1", Int16MultiArray, queue_size=1)
 
 
+    def callback_fn(self, data):
+        
+        print(data)
+        self.direction= data.data
+        self.gpt_input = 1
+        print(self.direction)
+
+
+    rospy.Subscriber("/arm_gpt", Int8, callback_fn)
+    
+    
+    
+    def run_gpt(self):
+        print("gpt_input : ", self.gpt_input)
+        
+        if(self.direction == 1):
+            self.vel_la1 = 150
+            self.msg.data = [self.vel_la1, self.vel_la2, self.var_st]
+            self.pub.publish(self.msg)
+            rospy.sleep(2)
+        if(self.direction == -1):
+            self.vel_la1 = -150
+            self.msg.data = [self.vel_la1, self.vel_la2, self.var_st]
+            self.pub.publish(self.msg)
+            rospy.sleep(2)
+    
+    
+    def callback(self, data):
+        xAxis1 = data.axes[0] 
+        yAxis1 = data.axes[1]
+        square = data.buttons[3]
+        circle = data.buttons[1]
+        self.vel_la1 = 0
+        self.vel_la2 = 0
+
+        # this part of if block, will tell which linear actuators to run and in which combo
+        # For eg: Both extending, one extending and other retracting, etc.
+        
+        if xAxis1 > 0:
+            self.vel_la1 = int((xAxis1)*255) # velocity of linear actuator 1 is proportional to how much you move the joystick (analog)
+        if xAxis1 < 0:
+            self.vel_la1 = int((xAxis1)*255)
+        if yAxis1 < 0:
+            self.vel_la2 = int((yAxis1)*255) # velocity of linear actuator 2 is proportional to how much you move the joystick (analog)
+        if yAxis1 > 0:
+            self.vel_la2 = int((yAxis1)*255)
+
+        # this part will tell if stepper should be enabled or disabled
+
+        if square == 1:
+            self.var_st = 1
+        elif circle == 1:
+            self.var_st = 2
+        else:
+            self.var_st = 0
+
+
+        if self.gpt_input:
+            self.run_gpt()
+            self.vel_la1 = 0
+            self.vel_la2 = 0
+        
+        self.msg.data = [self.vel_la1, self.vel_la2, self.var_st]
+        self.pub.publish(self.msg)
 
 if __name__ == '__main__':
-    rospy.Subscriber("/joy", Joy, callback)
-    while not rospy.is_shutdown():
-        msg = Int16MultiArray()  # Create a message instance
-        if xAxis1 > 0 and abs(xAxis1) > abs(yAxis1):
-            var = 1
-            vel = int(abs(xAxis1) * 255)
-        elif xAxis1 < 0 and abs(xAxis1) > abs(yAxis1):
-            var = 2
-            vel = int(abs(xAxis1) * 255)
-        elif yAxis1 > 0 and abs(yAxis1) > abs(xAxis1):
-            var = 3
-            vel = int(abs(yAxis1) * 255)
-        elif yAxis1 < 0 and abs(yAxis1) > abs(xAxis1):
-            var = 4
-            vel = int(abs(yAxis1) * 255)
-        elif square == 1:
-            var = 5
-            vel = 0
-        elif circle == 1:
-            var = 6
-            vel = 0
-        else:
-            run_gpt()
-            var = 0
-            vel = 0
+    rospy.init_node("joy_control", anonymous=True)
+    joystick_control = JoystickControl()
+    rospy.spin()
 
-        msg.data = [var, vel] 
-        pub.publish(msg)
-        

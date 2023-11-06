489338f25d6ffaf76c2fe840a0559a7ffb0829b8
Tushar2305 <119516424+Tushar2305@users.noreply.github.com>
2023-11-06 04:11:18 -0100

    Add files via upload

---
+++ groundwork/Vim/my_package/src/control_arm_sub.py
@@ -0,0 +1,101 @@
+
+#!/usr/bin/env python3
+
+import rospy
+from std_msgs.msg import Int16MultiArray, Int8
+from sensor_msgs.msg import Joy
+
+class JoystickControl:
+    def __init__(self):
+        self.vel_la1 = 0 # velocity of linear actuator 1 
+        self.vel_la2 = 0 # velocity of linear actuator 2
+        self.var_st = 0 # variable to enabel/disable stepper 
+        self.msg = Int16MultiArray()
+        
+        
+        
+        self.direction = 0
+        self.gpt_input = 0
+        
+        self.sub = rospy.Subscriber("/joy", Joy, self.callback)
+        self.pub = rospy.Publisher("/control1", Int16MultiArray, queue_size=1)
+        self.sub2 = rospy.Subscriber("/arm_gpt", Int8, self.callback_fn)
+
+
+
+    def callback_fn(self, data):
+        
+        print(data)
+        self.direction= data.data
+        self.gpt_input = 1
+        print(self.direction)
+
+
+    
+    
+    
+    def run_gpt(self):
+        print("gpt_input : ", self.gpt_input)
+        self.gpt_input = 0
+        
+        if(self.direction == 1):
+            self.vel_la1 = 200
+            self.vel_la2 = 51
+            self.msg.data = [self.vel_la1, self.vel_la2, self.var_st]
+            self.pub.publish(self.msg)
+            print("gpt",self.msg)
+            rospy.sleep(2)
+        if(self.direction == -1):
+            self.vel_la1 = -200
+            self.vel_la2 = 51
+            self.msg.data = [self.vel_la1, self.vel_la2, self.var_st]
+            print("gpt",self.msg)
+            self.pub.publish(self.msg)
+            rospy.sleep(2)
+    
+    
+    def callback(self, data):
+        xAxis1 = data.axes[0] 
+        yAxis1 = data.axes[1]
+        square = data.buttons[3]
+        circle = data.buttons[1]
+        self.vel_la1 = 0
+        self.vel_la2 = 0
+
+        # print(data)
+
+        # this part of if block, will tell which linear actuators to run and in which combo
+        # For eg: Both extending, one extending and other retracting, etc.
+        
+        if xAxis1 > 0:
+            self.vel_la1 = int((xAxis1)*255) # velocity of linear actuator 1 is proportional to how much you move the joystick (analog)
+        if xAxis1 < 0:
+            self.vel_la1 = int((xAxis1)*255)
+        if yAxis1 < 0:
+            self.vel_la2 = int((yAxis1)*255) # velocity of linear actuator 2 is proportional to how much you move the joystick (analog)
+        if yAxis1 > 0:
+            self.vel_la2 = int((yAxis1)*255)
+
+        # this part will tell if stepper should be enabled or disabled
+
+        if square == 1:
+            self.var_st = 1
+        elif circle == 1:
+            self.var_st = 2
+        else:
+            self.var_st = 0
+
+
+        if self.gpt_input:
+            self.run_gpt()
+            self.vel_la1 = 0
+            self.vel_la2 = 0
+        
+        self.msg.data = [self.vel_la1, self.vel_la2, self.var_st]
+        self.pub.publish(self.msg)
+
+if __name__ == '__main__':
+    rospy.init_node("joy_control", anonymous=True)
+    joystick_control = JoystickControl()
+    rospy.spin()
+

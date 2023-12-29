a0944021a68d9d31d4b4a4274fb104a67b6525c2
sathvikramakrishnan <118525507+sathvikramakrishnan@users.noreply.github.com>
2023-12-29 14:30:16 +0530

    Add files via upload

--- groundwork/sathvik/tkinter GUI/LD_gui.py
+++ groundwork/sathvik/tkinter GUI/LD_gui.py
@@ -11,11 +11,11 @@ from tkinter import ttk
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import (
      FigureCanvasTkAgg)
+from matplotlib import style
 import threading
 from datetime import datetime
 
 from  gui_stuff.msg import *
-
 
 # global coordinate varibles
 sensor_x = []
@@ -28,13 +28,19 @@ brad_y = []
 chloro_y = []
 
 
-# callback functions
+# backend functions
 def sensor_callback(data):
     global co2_y, ch4_y, co_y
     co2_y.append(data.co2)
     ch4_y.append(data.ch4)
     co_y.append(data.co)
     sensor_plot()
+
+def spectro_callback(data):
+    global brad_y, chloro_y
+    brad_y.append(data.brad)
+    chloro_y.append(data.chloro)
+    spectro_plot()
 
 def ros_thread():
     # Publishers
@@ -43,62 +49,79 @@ def ros_thread():
     
     # SUbscribers
     rospy.Subscriber("/sensors", sensor_msg, sensor_callback)
+    rospy.Subscriber("/spectrometer", spectro_msg, spectro_callback)
     rospy.spin()
 
 def close_func():
     exit()
 
-def sensor_plot():
-    # Clear previous widgets
-    for widget in can_frame_sensor.winfo_children():
-        widget.destroy()
 
-    # Generates x coordinate
-    global sensor_x
-    if len(sensor_x)==0:
-        x = sensor_x.copy()
-        sensor_x.append(0)
-        
-    elif len(sensor_x)==1:
-        global sensor_start 
-        sensor_start = datetime.now()
-        sensor_x.append(round((datetime.now() - sensor_start).total_seconds()))
-        x = sensor_x[1:]
-    
-    else:
-        sensor_x.append(round((datetime.now() - sensor_start).total_seconds()))
-        x = sensor_x[1:]
-    
-    # Clear previous plots
-    ax1.cla()
-    ax2.cla()
-    ax3.cla()
-    
-    # New plots
-    ax1.plot(x, co2_y, 'b^-')
+# frontend functions
+def sensor_rec_plot():
+    ax1.plot(sensor_x, co2_y, 'b^-')
     ax1.set_title('CO2 plot')
     ax1.set_xlabel('time')
     ax1.set_ylabel('ppm')
-    
-    ax2.plot(x, ch4_y, 'r^-')
+
+    ax2.plot(sensor_x, ch4_y, 'r^-')
     ax2.set_title('CH4 plot')
     ax2.set_xlabel('time')
     ax2.set_ylabel('ppm')
-    
-    ax3.plot(x, co_y, 'g^-')
+
+    ax3.plot(sensor_x, co_y, 'g^-')
     ax3.set_title('CO plot')
     ax3.set_xlabel('time')
     ax3.set_ylabel('ppm')
 
-    plt.tight_layout()
-
-    # Load plots into tkinter window inside canvas widget
-    canvas_sensor = FigureCanvasTkAgg(figure=sensor_fig, master=can_frame_sensor)
-    canvas_sensor.draw_idle()
-    canvas_sensor.get_tk_widget().pack(padx=35, pady=35)
+def sensor_plot():
+    global sensor_start
+    if len(sensor_x)==0:
+        sensor_start = datetime.now()
+        sensor_x.append(0)
+    else:
+        # sensor_x.append(round((datetime.now() - sensor_start).total_seconds()))
+        sensor_x.append(((datetime.now() - sensor_start).total_seconds()))
     
+    ax1.clear()
+    ax2.clear()
+    ax3.clear()
+    
+    sensor_rec_plot()
+    
+    canvas_sensor.draw_idle()
+    
+def spectro_rec_plot():
+    ax4.plot(spectro_x, brad_y, 'b^-')
+    ax4.set_title('Bradford assay')
+    ax4.set_xlabel('wavelength')
+    ax4.set_ylabel('absorbance')
+
+    ax5.plot(spectro_x, chloro_y, 'r^-')
+    ax5.set_title('Chlorophyll')
+    ax5.set_xlabel('wavelength')
+    ax5.set_ylabel('absorbance')
+
+def spectro_plot():
+    global spectro_start
+    if len(spectro_x)==0:
+        spectro_start = datetime.now()
+        spectro_x.append(0)
+    else:
+        spectro_x.append(((datetime.now() - spectro_start).total_seconds()))
+    
+    ax4.clear()
+    ax5.clear()
+    
+    spectro_rec_plot()
+    
+    canvas_spectro.draw_idle()
+
+# button click functions
 def sensor_button_click():
     flag_pub.publish(1)
+
+def spectro_button_click():
+    flag_pub.publish(2)
 
 rospy.init_node("LD_gui", anonymous=True)
 
@@ -110,6 +133,8 @@ ros_t.start()
 main_win = tk.Tk()
 main_win.title('Life Detection Window')
 # main_win.geometry('1600x900')
+
+style.use("ggplot")
 
 main_win.protocol('WM_DELETE_WINDOW', close_func)
 
@@ -131,16 +156,30 @@ notebook.add(digi_micro, text="Digital Microscope")
 notebook.pack(expand=True, fill='both')
 
 # sensor window
-can_frame_sensor = tk.Frame(sensors)
-can_frame_sensor.pack()
 sensor_fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(16, 4))
 
-# plots empty graph
-sensor_plot()
+sensor_rec_plot()
 
-# trigger for flag var
+canvas_sensor = FigureCanvasTkAgg(figure=sensor_fig, master=sensors)
+canvas_sensor.draw_idle()
+canvas_sensor.get_tk_widget().pack(padx=35, pady=35)
+
 sensor_button = tk.Button(master=sensors, text='Publish trigger', command=sensor_button_click)
 sensor_button.pack(padx=15, pady=25)
+
+# spectro window
+spectro_fig, (ax4, ax5) = plt.subplots(1, 2, figsize=(16, 4))
+
+spectro_rec_plot()
+
+canvas_spectro = FigureCanvasTkAgg(figure=spectro_fig, master=spectro)
+canvas_spectro.draw_idle()
+canvas_spectro.get_tk_widget().pack(padx=35, pady=35)
+
+spectro_button = tk.Button(master=spectro, text='Publish trigger', command=spectro_button_click)
+spectro_button.pack(padx=15, pady=25)
+
+plt.tight_layout()
 
 
 main_win.mainloop()

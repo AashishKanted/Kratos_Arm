09b81d82289744da9395272d3aba2b073b5ac9d3
Akash Bhosale <132154113+akashbhosale16@users.noreply.github.com>
2024-02-15 15:42:23 +0530

    Create Potentiometer.ino

---
+++ groundwork/Akash/URC/Kinematics/Potentiometer.ino
@@ -0,0 +1,27 @@
+#include <ros.h>
+#include <Servo.h>
+#include <geometry_msgs/Point.h>
+
+ros::NodeHandle nh;
+
+geometry_msgs::Point potentiometer;
+ros::Publisher pub("Potentiometer_values",&potentiometer);
+
+const int pot_1 = A0;
+const int pot_2 = A1;
+
+void setup() {
+  nh.initNode();
+  nh.advertise(pub);
+}
+
+void loop() {
+  potentiometer.x = analogRead(pot_1);
+  potentiometer.y = analogRead(pot_2);
+
+  pub.publish(&potentiometer);
+  nh.spinOnce();
+
+  pub.publish(&potentiometer);
+
+}

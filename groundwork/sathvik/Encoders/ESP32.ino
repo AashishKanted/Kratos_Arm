e8ea1ae06c4b48f592405008e8eb7ca8358bab0b
sathvikramakrishnan <118525507+sathvikramakrishnan@users.noreply.github.com>
2024-04-02 22:41:37 +0530

    encoder calib script
    works with one orange encoder
    needs conversion factor

---
+++ groundwork/sathvik/Encoders/ESP32.ino
@@ -0,0 +1,46 @@
+#include <ros.h>
+#include <std_msgs/Int16.h>
+
+ros::NodeHandle nh;
+
+std_msgs::Int16 msg;
+ros::Publisher pub("Orange_enc", &msg);
+
+volatile unsigned int temp, counter = 0; 
+
+void setup() {
+  nh.initNode();
+  nh.advertise(pub);
+  // Serial.begin (115200);
+  pinMode(27, INPUT_PULLUP);
+  pinMode(5, INPUT_PULLUP); 
+  attachInterrupt(digitalPinToInterrupt(27), ai0, RISING);
+  attachInterrupt(digitalPinToInterrupt(5), ai1, RISING);
+  }
+  void loop() {
+  if( counter != temp ){
+    msg.data = counter;
+    pub.publish(&msg);
+    // Serial.println (counter);
+  temp = counter;
+  }
+  // msg.data = counter;
+  // pub.publish(&msg);
+  // temp = counter;
+  delay(100);
+  nh.spinOnce();
+  }
+void ai0() {
+  if(digitalRead(5)==LOW) {
+  counter++;
+  }else{
+  counter--;
+  }
+}
+void ai1() {
+  if(digitalRead(27)==LOW) {
+  counter--;
+  }else{
+  counter++;
+  }
+}

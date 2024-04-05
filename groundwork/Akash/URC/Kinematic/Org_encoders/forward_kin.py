bb30ed062967ef6d68e4d7aa40ce8df1ef4cbc4f
sathvikramakrishnan <118525507+sathvikramakrishnan@users.noreply.github.com>
2024-04-05 23:14:50 +0530

    Add files via upload

--- groundwork/Akash/URC/Kinematic/Org_encoders/forward_kin.py
+++ groundwork/Akash/URC/Kinematic/Org_encoders/forward_kin.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
 import rospy
 import math
-from std_msgs.msg import Int16MultiArray
-from geometry_msgs.msg import Point
+from std_msgs.msg import Float32MultiArray
 
 l1 = float(384.811)
 l2 = float(409.149)
@@ -12,17 +11,17 @@ def callback(msg):
     angle_1 = msg.data[0]
     angle_2 = msg.data[1]
     
-    theta_1 = math.radians(theta_1)
+    theta_1 = math.radians(angle_1)
     theta_2 = math.radians(angle_2-180+angle_1)
     
-    coords = Point()
-    coords.x = l1*math.cos(theta_1) + l2*math.cos(theta_2)
-    coords.y = l1*math.sin(theta_1) + l2*math.sin(theta_2)
-    coords.z = 0
+    coords = Float32MultiArray()
+    x = l1*math.cos(theta_1) + l2*math.cos(theta_2)
+    y = l1*math.sin(theta_1) + l2*math.sin(theta_2)
+    coords.data = [x,y]
     coords_pub.publish(coords)
     
 
 rospy.init_node('forward_kin',anonymous=True)
-rospy.Subscriber('/angle_enc',Int16MultiArray ,callback=callback)
-coords_pub = rospy.Publisher('forward_kin', Point, queue_size=1)
+rospy.Subscriber('/angle_enc',Float32MultiArray ,callback=callback)
+coords_pub = rospy.Publisher('forward_kin', Float32MultiArray, queue_size=1)
 rospy.spin()

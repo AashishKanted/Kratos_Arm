39fa3b215325ade9546a0cb59c2f0d636dc6ba89
sathvikramakrishnan <118525507+sathvikramakrishnan@users.noreply.github.com>
2024-04-05 22:34:07 +0530

    Add files via upload

--- groundwork/sathvik/Encoders/encoder_calib.py
+++ groundwork/sathvik/Encoders/encoder_calib.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 
 import rospy
-from std_msgs.msg import Int16, Int16MultiArray
+from std_msgs.msg import Int16, Int16MultiArray, Float32MultiArray
 import time
 
-status = 1
+# status = 1
 
 prev_enc_1 = 0
 prev_enc_2 = 0
@@ -21,47 +21,60 @@ ang_1_ret = 0
 ang_2_ret = 0
 
 def callback_1(data):
-    # global enc_1
-    # enc_1 = data.data
     global prev_enc_1, enc_1
     prev_enc_1 = enc_1
     enc_1 = data.data
 
 def callback_2(data):
-    pass
+    global prev_enc_2, enc_2
+    prev_enc_2 = enc_2
+    enc_2 = data.data
 
 rospy.init_node('enc_calibration', anonymous=True)
-rospy.Subscriber("Orange_enc", Int16, callback_1)
+rospy.Subscriber("Orange_enc_1", Int16, callback_1)
 rospy.Subscriber("Orange_enc_2", Int16, callback_2)
 
 vel_pub = rospy.Publisher('/control1', Int16MultiArray, queue_size=1)
 feed_pub = rospy.Publisher('/feedback_topic', Int16MultiArray, queue_size=1)
-angle_pub = rospy.Publisher('/angle_enc', Int16MultiArray, queue_size=1)
+angle_pub = rospy.Publisher('/angle_enc', Float32MultiArray, queue_size=1)
 rate = rospy.Rate(10)
 
-while status:
-    start_t = time.time()
-    # publishes pwm value for 15 seconds
-    # enough time for full retraction
-    while (time.time()-start_t) < 5:
-        array = Int16MultiArray()
-        array.data = [255,255]
-        vel_pub.publish(array)
-        rate.sleep()
-    else:
-        status = 0
-        enc_ret_1 = enc_1
-        enc_ret_2 = enc_2
-        enc_arr = Int16MultiArray()
-        enc_arr.data = [enc_1, 0]
-        start_t = time.time()
-        while (time.time()-start_t) < 2:
-            feed_pub.publish(enc_arr)
-            rate.sleep()
+# while status:
+#     start_t = time.time()
+    # # publishes pwm value for 15 seconds
+    # # enough time for full retraction
+    # while (time.time()-start_t) < 1:
+    #     array = Int16MultiArray()
+    #     array.data = [255,255]
+    #     vel_pub.publish(array)
+    #     rate.sleep()
+    # while (time.time()-start_t) < 1:
+    #     array = Int16MultiArray()
+    #     array.data = [-255,-255]
+    #     vel_pub.publish(array)
+    #     rate.sleep()
+    # else:
+    #     status = 0
+    #     enc_ret_1 = enc_1
+    #     enc_ret_2 = enc_2
+    #     enc_arr = Int16MultiArray()
+    #     enc_arr.data = [enc_1, enc_2]
+    #     start_t = time.time()
+    #     while (time.time()-start_t) < 2:
+    #         feed_pub.publish(enc_arr)
+    #         rate.sleep()
 
 while not rospy.is_shutdown():
+    print(enc_1)
+    print(enc_2)
+    print(enc_1 - enc_ret_1)
+    print(enc_2 - enc_ret_2)
+    print('############')
     angle_1 = ang_1_ret + (enc_1 - enc_ret_1)*angle_factor
     angle_2 = ang_2_ret + (enc_2 - enc_ret_2)*angle_factor
-    angle_pub.publish(Int16MultiArray(data=[angle_1, angle_2]))
+    arr = Float32MultiArray()
+    arr.data = [angle_1, angle_2]
+    angle_pub.publish(arr)
+    rate.sleep()
 
 rospy.spin()

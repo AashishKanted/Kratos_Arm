060bd4ca363abf919b0220ba05c8d63f92785f73
sathvikramakrishnan <118525507+sathvikramakrishnan@users.noreply.github.com>
2024-04-05 22:35:07 +0530

    Add files via upload

--- groundwork/sathvik/Encoders/encoder_calib.py
+++ groundwork/sathvik/Encoders/encoder_calib.py
@@ -39,37 +39,8 @@ feed_pub = rospy.Publisher('/feedback_topic', Int16MultiArray, queue_size=1)
 angle_pub = rospy.Publisher('/angle_enc', Float32MultiArray, queue_size=1)
 rate = rospy.Rate(10)
 
-# while status:
-#     start_t = time.time()
-    # # publishes pwm value for 15 seconds
-    # # enough time for full retraction
-    # while (time.time()-start_t) < 1:
-    #     array = Int16MultiArray()
-    #     array.data = [255,255]
-    #     vel_pub.publish(array)
-    #     rate.sleep()
-    # while (time.time()-start_t) < 1:
-    #     array = Int16MultiArray()
-    #     array.data = [-255,-255]
-    #     vel_pub.publish(array)
-    #     rate.sleep()
-    # else:
-    #     status = 0
-    #     enc_ret_1 = enc_1
-    #     enc_ret_2 = enc_2
-    #     enc_arr = Int16MultiArray()
-    #     enc_arr.data = [enc_1, enc_2]
-    #     start_t = time.time()
-    #     while (time.time()-start_t) < 2:
-    #         feed_pub.publish(enc_arr)
-    #         rate.sleep()
-
 while not rospy.is_shutdown():
-    print(enc_1)
-    print(enc_2)
-    print(enc_1 - enc_ret_1)
-    print(enc_2 - enc_ret_2)
-    print('############')
+    
     angle_1 = ang_1_ret + (enc_1 - enc_ret_1)*angle_factor
     angle_2 = ang_2_ret + (enc_2 - enc_ret_2)*angle_factor
     arr = Float32MultiArray()
